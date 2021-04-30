2910416ad83a2d79109c68bac7f8859d9c1ca7bd
Serin Jeon <serin0837@yahoo.com>
2021-04-30 09:57:30 +0100

    add simple message proto and work

--- main.go
+++ main.go
@@ -2,19 +2,22 @@ package main
 
 import (
 	"fmt"
-	"go-protocol-buffer/src/person/personpb"
+	"go-protocol-buffer/src/simple/simplepb"
 )
 
 func main() {
 	doSimple()
-	fmt.Print("hello")
 }
 
 func doSimple() {
-	sm := &personpb.Person{
-		Name:  "serin",
-		Id:    2,
-		Email: "dd",
+	sm := simplepb.SimpleMessage{
+		Id:         12345,
+		IsSimple:   true,
+		Name:       "my name",
+		SampleList: []int32{1, 4, 7, 8},
 	}
-	fmt.Print(sm)
+	fmt.Println(sm)
+	sm.Name = "I rename you"
+	fmt.Println(sm)
+	fmt.Println("The Id is:", sm.GetId())
 }
